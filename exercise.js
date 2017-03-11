18bee92c3fd4ca7a77de0a100713224c4ff03c7f
Jason <Jason@Colins-MacBook-Pro.local>
2017-03-10 19:39:17 -1000

    Completed #5

--- exercise.js
+++ exercise.js
@@ -13,15 +13,16 @@
 /*1. Mr. Bonner (aka the Red Rocket, the Red Mamba) doesn't belong in this class of Hall Of Famers. Remove this imposter immediately and assign it to a new variable named `notHof` Console.log this new variable as well as the original list.*/ 
 
 var hof = ["Jordan", "Bird", "Kobe", "Duncan", "Iverson", "Bonner"];
-
-
+var notHof = hof.pop();
+console.log(notHof);
 
 /* The array.push() adds one or more element to the end of an array and returns the new length of the array*/
 
 /*2. You've been tasked to do this week's grocery shopping. As you arrive at Times Supermarket, Kanye pings you to get a bottle of Hendricks gin. Add this to the existing shoppingList and console.log it.*/
 
 var shoppingList = ["cool ranch doritos", "kings hawaiian sweet bread", "peanut butter oreos", "fruit loops cereal"];
-
+//shoppingList.push("Hendricks gin");
+console.log(shoppingList.push("Hendricks gin"));
 
 
 
@@ -30,7 +31,7 @@ var shoppingList = ["cool ranch doritos", "kings hawaiian sweet bread", "peanut 
 /*3. Use the force, or in this case the reverse method to help Yoda make some sense with his motivational talk to the young Jedi interns.*/
 
 var yoda = ["try", "no", "is", "there", "not", "do", "or", "do"];
-
+console.log(var.reverse());
 
 
 /*The array.shift() removes the first element from an array and returns that element*/
@@ -38,14 +39,14 @@ var yoda = ["try", "no", "is", "there", "not", "do", "or", "do"];
 /*4. People been lining up for hours to get the newest iphone release. Help manage the unruly crowd of privileged customers by serving them one at a time and assiging it to a variable named `nowServing`. Console.log this new variable as well as the waitList.*/
 
 var waitList = ["Chance the Rapper", "Khalid", "Tay-Tay", "Barry Manilow", "Piko Taro"];
-
+console.log(waitList.shift())
 
 
 /*The array.unshift() adds one or more elements to the front of an array and returns the new length of the array*/
 
 /*5. Apparently, you didn't do a good job with crowd control in the previous exercise. Kanye just cut in front of everyone!. Add Kanye to the waitList array above. Console.log waitList*/
 
-
+console.log(waitList.unshift("Kanye"));
 
 /*The array.splice() adds and/or removes elements from an array.
 array.splice(index, how many items to be removed)*/
